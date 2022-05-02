d3ca24bb2b797a46ba41460c18da391ce331d988
SvitlanaMO <svictory999@gmail.com>
2022-05-02 21:57:25 +0200

    Files amended as per requirements

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() { // this function will compute the interest rate
+    let principal = document.getElementById("principal").value;
+     
+    if (principal <= 0) { // use if statement to check validation
+        alert("Enter a positive number!");
+        document.getElementById("principal").focus();
+        result.innerHTML = "";
+        return;
+    }
+
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    let interest = principal * years * rate / 100;
+    let year = new Date().getFullYear() + parseInt(years);  
+    let result = document.getElementById("result");
+    result.innerHTML =  "If you deposit <mark>" + principal + 
+                        "</mark>,<br/>at an interest rate of <mark>" + rate + 
+                        "%</mark><br/>You will receive an amount of <mark>" + interest + 
+                        "</mark>,<br/>in the year <mark>" + year + "</mark><br/>";                    
 }
-        
+
+function updateRate() {
+    let rateval = document.getElementById("rate").value;
+    document.getElementById("rate_value").innerText = rateval;
+}
